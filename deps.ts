edb8e603da2a28b54c48590f9da6d2e93f0730b0
Ed Ropple <ed@edropple.com>
2023-05-22 23:00:52 -0400

    deps update

--- deps.ts
+++ deps.ts
@@ -1,16 +1,16 @@
-export { Type } from "https://deno.land/x/typebox@0.24.11/src/typebox.ts";
-export type { Static } from "https://deno.land/x/typebox@0.24.11/src/typebox.ts";
+export { Type } from "https://deno.land/x/typebox@0.28.11/src/typebox.ts";
+export type { Static } from "https://deno.land/x/typebox@0.28.11/src/typebox.ts";
 
-export { default as Ajv } from "https://esm.sh/ajv@8.11.0";
+export { default as Ajv } from "https://esm.sh/ajv@8.12.0";
 export { default as AjvFormats } from "https://esm.sh/ajv-formats@2.1.1";
 
-import { Command as CliffyCommand, CompletionsCommand } from "https://deno.land/x/cliffy@v0.24.2/mod.ts";
+import { Command as CliffyCommand, CompletionsCommand } from "https://deno.land/x/cliffy@v0.25.7/mod.ts";
 export { CliffyCommand, CompletionsCommand };
 
 export { default as YAML } from "https://esm.sh/js-yaml@4.1.0";
 
-export * as Path from "https://deno.land/std@0.146.0/path/mod.ts";
+export * as Path from "https://deno.land/std@0.188.0/path/mod.ts";
 
 export { default as Shlex } from "https://esm.sh/shlex@2.1.2";
 
-export { sleep } from "https://deno.land/x/sleep@v1.2.0/mod.ts";
+export { sleep } from "https://deno.land/x/sleep@v1.2.1/mod.ts";
